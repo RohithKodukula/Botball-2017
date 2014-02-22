d9343f747031a27b9a8a4c924c3297aeb5411ace
Joshua Send <joshuasend1@gmail.com>
2014-02-22 11:07:01 -0800

    fixed hub

---
+++ Legobot/CPP/legobotMain.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <kovan/motors.hpp>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	Motor spinner(0);
+	Motor lWheel(1);
+	Motor rWheel(3);
+	
+	rWheel.motor(60);
+	lWheel.motor(-60);
+	spinner.moveAtVelocity(600);
+	sleep(20);
+}
+
+
+class DriveMotor {
+	public:
+		void DeviceMotor(int motorPort)  {
+			
+		};
+		
+		void setSpeedMultiplier(float multiplier) {
+			speedMultiplier = multiplier;
+		};
+		
+		float getSpeedMultiplier() {
+			return speedMultiplier;
+		};
+		
+	private:
+		float speedMultiplier;
+		Motor motor;
+		
