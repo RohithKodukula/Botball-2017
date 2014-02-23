7d2c553f8e8193dfcbe30046e5b01a35812508f8
Joshua Send <joshuasend1@gmail.com>
2014-02-22 19:17:56 -0800

    daily updated

--- Legobot/LegobotCPP/legobotMain.cpp
+++ Legobot/LegobotCPP/legobotMain.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <kovan/motors.hpp>
+#include <kovan/motors.h>
 
 #include "legobotFunctions.cpp"
 
 using namespace std;
 
+
+class MyMotor: public Motor {
+	public:
+		MyMotor(int port): Motor(port) {
+			tmp = 400;
+		};
+		
+		int getPosition() {
+			return get_motor_position_counter(port());
+		};
+	private:
+		int tmp;
+
+};
+
 int main(int argc, char *argv[])
 {
-	Motor spinner(0);
+	const int x = 0;
+	MyMotor spinner(x);
 	Motor lWheel(1);
 	Motor rWheel(3);
 	
 	rWheel.motor(60);
 	lWheel.motor(-60);
 	spinner.moveAtVelocity(600);
-	sleep(20);
+	
+	for (int i = 0; i < 100; i++) {
+		cout << spinner.getPosition() << endl;
+	};	
+	
+	sleep(5);
+	cout << spinner.getPosition() << endl;
+	return 0;
 }
 
 
-class DriveMotor {
+/*
+class DriveMotor: public Motor {
 	public:
-		void DeviceMotor(int motorPort)  {
-			
+		void DeviceMotor(int port)  {
+			Motor motor(port);
 		};
 		
 		void setSpeedMultiplier(float multiplier) {
@@ -34,5 +59,7 @@ class DriveMotor {
 		
 	private:
 		float speedMultiplier;
-		Motor motor;
-		
+		int motorPort;
+		Motor motor(motorPort);
+}
+*/
