4a5edd61325084083d4aa8612f768c48c47e162b
Gita5683 <gita.pm1@gmail.com>
2014-02-28 17:02:15 -0800

    LOL

--- PracticeBot/PracticebotC/practiceMain.c
+++ PracticeBot/PracticebotC/practiceMain.c
@@ -1,14 +1,21 @@
 int R_MOTOR = 0;
 int L_MOTOR = 2;
 int main() {	
-	moveStraight();
+	moveStraight(66.6);
 }
 
-void moveStraight() {
-motor(R_MOTOR , -50);
-motor(L_MOTOR , -50);
-msleep(5000);
-ao();
+void moveStraight(double distance) {
+	
+	clear_motor_position_counter(R_MOTOR);
+	double ticksPerCentimeter = 75.35;
+	double ticks = 75.35 * distance;
+	printf("Ticks: %d\n", ticks);
+	motor(R_MOTOR , 49);
+	motor(L_MOTOR , 50);
+	printf("Target position: %d\n", 75*66);
+	while ((double)get_motor_position_counter(R_MOTOR) < ticks) {
+		msleep(50);
+		printf("%d\n",get_motor_position_counter(R_MOTOR));
+	}
+	ao();
 }
-
-
