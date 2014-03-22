2747db8c3247af37b8f497064a9f644019e5868b
Gita5683 <gita.pm1@gmail.com>
2014-03-21 17:32:52 -0700

    legobot main routine update

--- Legobot/LegobotC/legobotMain.c
+++ Legobot/LegobotC/legobotMain.c
@@ -1,4 +1,4 @@
-#include "legoBotConstants.h"
+#include "legobotConstants.h"
 #include "legobotFunctions.h"
 //192.168.0.148
 
@@ -67,15 +67,7 @@ int main() {
 			//pivotOnRight(NORMAL_SPEED, -45);
 		}
 		else if (b_button()) {
-<<<<<<< HEAD
-			raiseArm();
-			msleep(500);
-			moveArm(-600);
-			sleep(500);
-
-=======
 			arcToWallAlign(NORMAL_SPEED, 1, 1);
->>>>>>> f5416f8d80ea688b6c5714fb33a63a5f159d82ea
 			/*enable_servo(HANGER_SERVO);
 			set_servo_position(HANGER_SERVO,HANGER_BACK);
 			msleep(2000);
@@ -98,34 +90,26 @@ int main() {
 			//arcToWallAlign(NORMAL_SPEED+10, 1, 1);
 			//arcToTouch(NORMAL_SPEED, 1, 1);
 			//pivotOnRightTillLeftTouch(NORMAL_SPEED, 1);
-			moveToTouch(NORMAL_SPEED);
+			
+			
+			//wall align
+			moveToTouch(NORMAL_SPEED); 
 			pivotOnLeft(NORMAL_SPEED, -60);
 			moveToDist(NORMAL_SPEED, -5);
 			pivotOnRight(NORMAL_SPEED, -60);
 			moveToDist(NORMAL_SPEED,-9);
 			turnTest(-87.5);
 			
-<<<<<<< HEAD
-			moveToDistWithDipstick(SLOW_SPEED, 50);
-			turnTest(-86.4);
-			moveToDistWithDipstick(SLOW_SPEED, 61);
-			turnTest(88.36);
-=======
-			moveToDistWithDipstick(SLOW_SPEED, 55);
+			moveToDistWithDipstick(SLOW_SPEED, 46);
 			turnTest(-87.5);
 			moveToDistWithDipstick(SLOW_SPEED, 68);
 			turnTest(88.4);
->>>>>>> f5416f8d80ea688b6c5714fb33a63a5f159d82ea
 			msleep(1000);
 			raiseArm();
 			msleep(500);
 			setHangerClawPosition(HANGER_UP);
-<<<<<<< HEAD
-			moveToDist(NORMAL_SPEED, 29.5);
-=======
 			moveToWallAlign(NORMAL_SPEED);
 			msleep(500);
->>>>>>> f5416f8d80ea688b6c5714fb33a63a5f159d82ea
 			setHangerClawPosition(HANGER_FORWARD);
 			msleep(500);
 			thread_destroy(spinnerThread);
@@ -135,18 +119,39 @@ int main() {
 			moveToDist(NORMAL_SPEED, -5);
 			msleep(100);
 			pivotOnLeft(NORMAL_SPEED, -90);
-<<<<<<< HEAD
-=======
 			moveToDist(NORMAL_SPEED, 5);
 			resetArm();
-			pivotOnLeft(NORMAL_SPEED, 90);
+			//pivotOnLeft(NORMAL_SPEED, 90);
 			//turnTest(-87.5);
+			
+			moveToDist(NORMAL_SPEED, -20);
+			turnTest(-90);
+			moveToDist(30);
+			turnTest(90);
+			moveToDist(NORMAL_SPEED, 20);
+			setHangerClawPosition(HANGER_UP);
+			setHangerClawPosition(HANGER_FORWARD);
+			moveToDist(NORMAL_SPEED, -15);
+			raiseArm();
+			msleep(500);
+			moveArm(-90);
+			moveToDist(NORMAL_SPEED, 15);
+			
+			raiseArm();
+			msleep(500);
+			moveArm(90);
+			
+			
+			
+			
+			
+			
+			
 			
 			//moveToDist(NORMAL_SPEED,-5);
 			//pivotOnleft(NORMAL_SPEED, -30);
 			//pivotOnRight(NORMAL_SPEED, 90);
 			
->>>>>>> f5416f8d80ea688b6c5714fb33a63a5f159d82ea
 		
 		}
 		msleep(100);
