0101988539688c63726cc76229347ad762855280
Ben <blinternetgeek@gmail.com>
2014-03-24 17:49:28 -0700

    create arm raising, stupid #2

---
+++ Createbot/CreatebotC/Createbot2/CreatebotC/createVision.h
@@ -0,0 +1,15 @@
+#ifndef _CREATEVISION_H_
+#define _CREATEVISION_H_
+
+//camera
+void centerCamera(int channel, int object);
+int centerCameraFast(int channel);
+
+void cameraInitialize();
+
+void blobTrack(int channel, int object);
+int getLargestBlob(int channel);
+
+int cameraSeesBigOrange();
+
+#endif
---
+++ Createbot/CreatebotC/Createbot2/CreatebotCPP/createMain.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <kovan/motors.hpp>
+
+#include "createFunctions.cpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+}
